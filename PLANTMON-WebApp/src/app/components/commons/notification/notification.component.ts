97981112ce9ecca40f0be4bed0962fa7d827d839
Melania Morales <melaniacrc@gmail.com>
2021-06-04 13:06:12 -0600

    U: Home content

--- PLANTMON-WebApp/src/app/components/commons/notification/notification.component.ts
+++ PLANTMON-WebApp/src/app/components/commons/notification/notification.component.ts
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
+  today: number = Date.now();
+  records=[
+    {
+      reason: "Regar planta dos mil novecientas veces para que no se muera",
+      day: ["Lunes, Martes"],
+      repeat: true
+    },
+    {
+      reason: "Abonar planta para ver si crece mas que un sapo parado de manos",
+      day: ["Lunes, Martes"],
+      repeat: true
+    },
+    {
+      reason: "Podar planta para que quede como joaquin cuando se corta el pelo, con una mera jupa",
+      day: ["Lunes, Martes"],
+      repeat: true
+    },
+    {
+      reason: "Correr la planta del sol para que no se queme como mi pobre cerebro en este preciso instante, ayuda por favor",
+      day: ["Lunes, Martes"],
+      repeat: true
+    }
+  ]
 
   constructor() { }
 
