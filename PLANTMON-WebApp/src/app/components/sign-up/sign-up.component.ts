106353ed6431404840627d004420f11b5e91b62d
Melania Morales <melaniacrc@gmail.com>
2021-06-06 19:14:29 -0600

    U: Sign up forms and routing

--- PLANTMON-WebApp/src/app/components/sign-up/sign-up.component.ts
+++ PLANTMON-WebApp/src/app/components/sign-up/sign-up.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  correo: string = "";
+  password: string = "";
+  fechaNacimiento: Date;
+  tipo: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  sendData(userSignUpForm){
+    this.correo = userSignUpForm.form.value.email;
+    this.password = userSignUpForm.form.value.password;
+    this.fechaNacimiento = userSignUpForm.form.value.date;
+    this.tipo = userSignUpForm.form.value.tipo;
+    console.log(this.correo);
+    console.log(this.password);
+    console.log(this.fechaNacimiento);
+    console.log(this.tipo);
   }
 
 }
